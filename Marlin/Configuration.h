db9b9a66aef41da4f6832273f1ce78a13f7f6926
Jukka Kinnunen <jukka.kinnunen@iki.fi>
2013-07-18 20:04:12 +0300

    Right offsets for Z auto config probe.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -303,7 +303,7 @@ const bool Z_ENDSTOPS_INVERTING = false; // set to true to invert the logic of t
 #define NUM_AXIS 4 // The axis order in all axis related arrays is X, Y, Z, E
 #define HOMING_FEEDRATE {100*60, 100*60, 100*60, 0}  // set the homing speeds (mm/min)
 
-#define Z_PROBE_OFFSET {0, -13.6, -6.4, 0}  // Distance between hotend nozzle and deployed bed leveling probe.
+#define Z_PROBE_OFFSET {-35.0, 17.0, -4.5, 0}  // Distance between hotend nozzle and deployed bed leveling probe.
 
 // default settings 
 
